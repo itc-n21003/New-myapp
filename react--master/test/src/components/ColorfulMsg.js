d2d0e06425d300f428d3e84fc8a69767a89a7041
yuuki <n21003@std.it-college.ac.jp>
2021-12-13 09:43:35 +0900

    reactapi

--- react--master/test/src/components/ColorfulMsg.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
- 
-const ColorfulMsg = props => {
-  return <p style={{ color: props.color}}>{props.children}</p>;
-};
- 
-export default ColorfulMsg;
--- react--master/test/src/components/Msg.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Msg = () => {
-    return <p style={{ color: "blue" }}>Reactは難しくないよー</p>;
-  };
-
-export default Msg;
